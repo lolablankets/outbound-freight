0c195429ee9577995a5551b22abf3a3b9034eb8a
Isaac Lyon <isaaclyon@gmail.com>
2025-09-09 22:38:44 +0000

    clean up

--- outbound_freight/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Outbound Freight Invoice Processing Pipeline
-
-Process shipping invoices from FedEx, UPS, and USPS to calculate 
-weighted average shipping costs per product.
-"""
-
-__version__ = "0.1.0"
